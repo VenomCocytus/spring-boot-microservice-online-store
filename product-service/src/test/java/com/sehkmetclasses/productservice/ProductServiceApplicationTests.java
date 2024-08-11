307f6521daf36b5b0aa758b7a13db75b6543eb53
VenomCocytus <nef_allan9704@yahoo.com>
2024-08-12 00:25:07 +0100

    first commit

---
+++ product-service/src/test/java/com/sehkmetclasses/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehkmetclasses.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
