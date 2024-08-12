0898d9dd4a4ed361354e411a4e1afccdbf5a9641
allan-elong <allan.elong@bjft.ca>
2024-08-12 17:19:00 +0100

    Update - Fixing commit warnings

--- product-service/src/main/java/com/sehkmet/microservices/productservice/command/controller/ProductCommandController.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/command/controller/ProductCommandController.java
@@ -2,7 +2,6 @@ package com.sehkmet.microservices.productservice.command.controller;
 
 import com.sehkmet.microservices.productservice.command.dto.CreateProductRequestDTO;
 import com.sehkmet.microservices.productservice.command.service.ProductService;
-import com.sehkmet.microservices.productservice.model.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
--- product-service/src/main/java/com/sehkmet/microservices/productservice/command/service/ProductService.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/command/service/ProductService.java
@@ -1,7 +1,6 @@
 package com.sehkmet.microservices.productservice.command.service;
 
 import com.sehkmet.microservices.productservice.command.dto.CreateProductRequestDTO;
-import com.sehkmet.microservices.productservice.model.Product;
 
 public interface ProductService {
 
