cd9e6cdcbc7d37b0be66b204d68494e9fba0fc71
allan-elong <allan.elong@bjft.ca>
2024-08-14 17:15:31 +0100

    Update - Deleting the common service

--- common/src/test/java/com/sehkmet/microservices/common/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.sehkmet.microservices.common;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
