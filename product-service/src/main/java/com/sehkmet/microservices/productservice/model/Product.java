c51a6ec1274941ba201c7a90087855317f1b7bce
allan-elong <allan.elong@bjft.ca>
2024-09-10 18:14:10 +0100

    Updating - Fixing commit warnings

--- product-service/src/main/java/com/sehkmet/microservices/productservice/model/Product.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/model/Product.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
