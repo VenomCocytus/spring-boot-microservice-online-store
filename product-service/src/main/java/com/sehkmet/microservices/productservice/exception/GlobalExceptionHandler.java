7038b49aa17dee7c044b7dc1ad156fa76af2f9e6
allan-elong <allan.elong@bjft.ca>
2024-09-21 02:19:18 +0100

    Update - Fixing commit warnings

--- product-service/src/main/java/com/sehkmet/microservices/productservice/exception/GlobalExceptionHandler.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/exception/GlobalExceptionHandler.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.io.FileNotFoundException;
 import java.util.Set;
 
 @RestControllerAdvice
--- product-service/src/main/java/com/sehkmet/microservices/productservice/query/service/impl/ProductQueryServiceImpl.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/query/service/impl/ProductQueryServiceImpl.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.FileSystemNotFoundException;
 import java.util.List;
 @Service
 @Slf4j
