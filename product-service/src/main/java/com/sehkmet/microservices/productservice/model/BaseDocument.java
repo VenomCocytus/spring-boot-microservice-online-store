d787c6f82d132bfc441b3259fcde743f584404bc
allan-elong <allan.elong@bjft.ca>
2024-09-22 22:17:38 +0100

    Update - Fixing commit warnings

--- product-service/src/main/java/com/sehkmet/microservices/productservice/model/BaseDocument.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/model/BaseDocument.java
@@ -1,9 +1,7 @@
 package com.sehkmet.microservices.productservice.model;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.data.annotation.Id;
 
 @Getter
--- product-service/src/main/java/com/sehkmet/microservices/productservice/model/Product.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/model/Product.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
