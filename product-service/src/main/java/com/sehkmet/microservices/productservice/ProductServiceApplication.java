c1abdaccd18ac4d31745b1ec43e8dd6b6b049a5b
allan-elong <allan.elong@bjft.ca>
2024-12-04 17:14:44 +0100

    Update - Resolving source message error

--- product-service/src/main/java/com/sehkmet/microservices/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/ProductServiceApplication.java
@@ -3,7 +3,7 @@ package com.sehkmet.microservices.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.sehkmet.common-library")
+@SpringBootApplication(scanBasePackages = "com.sehkmet")
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
