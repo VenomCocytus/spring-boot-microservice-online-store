619d240cc595b1df32575f93151fcd32a8e92a6c
allan-elong <allan.elong@bjft.ca>
2024-12-06 17:21:06 +0100

    Update - Fixing commit warnings

--- inventory-service/src/main/java/com/sehkmet/microservices/inventoryservice/command/controller/InventoryCommandController.java
+++ inventory-service/src/main/java/com/sehkmet/microservices/inventoryservice/command/controller/InventoryCommandController.java
@@ -3,8 +3,6 @@ package com.sehkmet.microservices.inventoryservice.command.controller;
 import com.sehkmet.microservices.inventoryservice.command.dto.VerifyStockRequest;
 import com.sehkmet.microservices.inventoryservice.command.service.InventoryCommandService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
