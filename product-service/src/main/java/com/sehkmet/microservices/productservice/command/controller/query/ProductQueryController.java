03d7dcf9f4cf12a58bb52bb13d20e6f64d742b13
allan-elong <allan.elong@bjft.ca>
2024-12-16 11:04:26 +0100

    Update - Adding open api documentation for the product query controller

--- product-service/src/main/java/com/sehkmet/microservices/productservice/command/controller/query/ProductQueryController.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/command/controller/query/ProductQueryController.java
@@ -5,10 +5,16 @@ import com.sehkmet.microservices.productservice.command.dto.query.response.GetPr
 import com.sehkmet.microservices.productservice.command.service.query.ProductQueryService;
 import com.sehkmet.microservices.productservice.exception.runtime.ProductNotFoundException;
 import com.sehkmet.microservices.productservice.validation.annotation.ProductIdPathVariableExists;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +32,24 @@ public class ProductQueryController {
 
     private final ProductQueryService productQueryService;
 
+    @Operation(
+            summary = "Retrieve all products",
+            description = "This endpoint retrieves a list of all products available in the inventory."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Products retrieved successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GenericResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error"
+            )
+    })
     @GetMapping("/all")
     public ResponseEntity<GenericResponse<List<GetProductResponse>>> getAllProduct() {
 
@@ -36,6 +60,36 @@ public class ProductQueryController {
                         translate("success.products-retrieved-successfully")));
     }
 
+    @Operation(
+            summary = "Retrieve product details",
+            description = "This endpoint retrieves detailed information about a specific product identified by its ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "302",
+                    description = "Product found successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GenericResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Product not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductNotFoundException.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            )
+    })
     @GetMapping("/{productId}")
     public ResponseEntity<GenericResponse<GetProductResponse>> getProductDetails(
             @PathVariable
