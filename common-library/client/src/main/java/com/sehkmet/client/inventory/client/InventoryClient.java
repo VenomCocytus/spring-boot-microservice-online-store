eb001ba67f24826470d839fb456638062803ffcf
allan-elong <allan.elong@bjft.ca>
2024-12-16 12:20:30 +0100

    Update - Successfully added a circuit breaker in the gateway api

--- common-library/client/src/main/java/com/sehkmet/client/inventory/client/InventoryClient.java
+++ common-library/client/src/main/java/com/sehkmet/client/inventory/client/InventoryClient.java
@@ -2,6 +2,8 @@ package com.sehkmet.client.inventory.client;
 
 import com.sehkmet.client.inventory.dto.VerifyStockRequest;
 import com.sehkmet.core.common.GenericResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
