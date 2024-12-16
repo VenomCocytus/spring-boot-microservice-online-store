0eec47928913f75696dce37bbafc2f73ce628d86
allan-elong <allan.elong@bjft.ca>
2024-12-16 11:01:21 +0100

    Update - Adding open api documentation for the product command controller

--- product-service/src/main/java/com/sehkmet/microservices/productservice/command/controller/command/ProductCommandController.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/command/controller/command/ProductCommandController.java
@@ -4,12 +4,20 @@ import com.sehkmet.core.common.GenericResponse;
 import com.sehkmet.microservices.productservice.command.dto.command.request.CreateProductRequest;
 import com.sehkmet.microservices.productservice.command.dto.command.response.CreateProductResponse;
 import com.sehkmet.microservices.productservice.command.service.command.ProductCommandService;
+import com.sehkmet.microservices.productservice.exception.runtime.ProductNotFoundException;
 import com.sehkmet.microservices.productservice.validation.annotation.ProductIdPathVariableExists;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import static com.sehkmet.utils.utils.Utils.translate;
@@ -22,6 +30,36 @@ public class ProductCommandController {
 
     private final ProductCommandService productCommandService;
 
+    @Operation(
+            summary = "Create a new product",
+            description = "This endpoint allows you to create a new product in the inventory."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Product created successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GenericResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid input data",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ConstraintViolationException.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            )
+    })
     @PostMapping("/create")
     public ResponseEntity<GenericResponse<CreateProductResponse>> createProduct(
             @Valid
@@ -36,6 +74,32 @@ public class ProductCommandController {
                 ));
     }
 
+    @Operation(
+            summary = "Delete a product",
+            description = "This endpoint allows you to delete a product from the inventory by its ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Product deleted successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Product not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductNotFoundException.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<GenericResponse<?>> deleteProduct(
             @ProductIdPathVariableExists
--- product-service/src/main/java/com/sehkmet/microservices/productservice/command/dto/command/request/CreateProductRequest.java
+++ product-service/src/main/java/com/sehkmet/microservices/productservice/command/dto/command/request/CreateProductRequest.java
@@ -1,22 +1,31 @@
 package com.sehkmet.microservices.productservice.command.dto.command.request;
 
 import com.sehkmet.microservices.productservice.validation.annotation.ProductNameNotAlreadyExists;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.AUTO;
+
 public record CreateProductRequest(
+        @Schema(description = "The name of the product", example = "An awesome product", requiredMode = AUTO)
         @NotBlank(message = "{messages.product-name-validation-not-blank-alert}")
         @Size(min = 3, max = 50,
                 message = "{messages.product-name-validation-size-alert}")
         @ProductNameNotAlreadyExists
         String name,
+        @Schema(description = "The description of the product", example = "This is an amazing product", requiredMode = AUTO)
+        @Size(min = 3, max = 500,
+                message = "{messages.product-description-validation-size-alert}")
         @NotBlank(message="{messages.product-description-validation-not-blank-alert}")
         @Size(min = 3, max = 500,
                 message = "{messages.product-description-validation-size-alert}")
         String description,
+
+        @Schema(description = "The price of the product", example = "19.99", requiredMode = AUTO)
         @NotNull(message = "{messages.product-price-validation-not-null-alert}")
         BigDecimal price
 ) {}
